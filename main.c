20741f57cef30c5a258316cfbff3a513708b56fb
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-05-24 07:18:31 -0700

    changes

--- main.c
+++ main.c
@@ -64,7 +64,7 @@ int main(UNUSED int ac, UNUSED char **av, char **env)
 
 	while (run_flag)
 	{
-		_printf("#cisfun$ ");
+		_printf("# ");
 		num_chars = getline(&buffer, &buf_size, stdin);
 		if (num_chars == 1)
 			continue;
