7bc03ad8c92df742395170251821da0a08438156
AhmedAAN <109803368+AhmedAAN@users.noreply.github.com>
2023-05-27 02:25:06 +0300

    Update process.c

--- process.c
+++ process.c
@@ -11,32 +11,35 @@ void process(char *filepath, char *args[MAX_ARGUMENTS], char **env)
 	pid_t pid;
 	int status;
 
-	pid = fork();
-	if (pid == 0)
+	if (access(filepath, X_OK) == 0)
 	{
-		if (access(filepath, X_OK) == 0)
+		pid = fork();
+		if (pid == 0)
 		{
-			execve(filepath, args, env);
-			perror("execve");
+			if (access(filepath, X_OK) == 0)
+			{
+				execve(filepath, args, env);
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				_printf(args[0]);
+				_printf(": command not found");
+				_putchar('\n');
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			_printf(args[0]);
-			_printf(": command not found");
-			_putchar('\n');
-			exit(EXIT_FAILURE);
+			do {
+				waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 		}
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		do {
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 }
