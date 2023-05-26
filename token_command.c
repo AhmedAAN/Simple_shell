f9d57ac83d1217371ff1a1e2e83e9dbdc365cc90
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-05-26 14:51:40 -0700

    _strtok() added

--- token_command.c
+++ token_command.c
@@ -1,5 +1,47 @@
 #include "shell.h"
 
+
+/**
+ * _strtok - Tokenizes a string into tokens
+ * @str: The string to tokenize
+ * @delimiters: The delimiter characters
+ * Return: A pointer to the next token, or NULL if no more tokens are found
+ */
+char *_strtok(char *str, const char *delimiters)
+{
+	static char *input = NULL;
+	char *token;
+
+	if (str != NULL)
+	{
+		input = str;
+	}
+
+	if (input == NULL)
+	{
+		return (NULL);
+	}
+	token = input;
+	input += strspn(input, delimiters);
+	if (*input == '\0')
+	{
+		input = NULL;
+		return (NULL);
+	}
+	input += strcspn(input, delimiters);
+	if (*input == '\0')
+	{
+		input = NULL;
+	}
+	else
+	{
+		*input++ = '\0';
+	}
+
+	return (token);
+}
+
+
 /**
  * token_command - tokenize the command into the args array
  *
@@ -13,7 +55,7 @@ int token_command(char *buffer, char **args)
 	int num_args = 0;
 	char *arg;
 
-	arg = strtok(buffer, " \n");
+	arg = _strtok(buffer, " \n");
 
 	while (arg != NULL && num_args < MAX_ARGUMENTS - 1)
 	{
@@ -22,7 +64,7 @@ int token_command(char *buffer, char **args)
 			args[num_args] = arg;
 		}
 		num_args++;
-		arg = strtok(NULL, " \n");
+		arg = _strtok(NULL, " \n");
 	}
 
 	args[num_args] = NULL;
